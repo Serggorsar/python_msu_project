f593ed8027bd4ec14e5e36c2b515e193a2bea1e2
Serggorsar <480980@mail.ru>
2020-08-02 01:00:23 +0300

    refactoring

--- extraction.py
+++ extraction.py
@@ -8,6 +8,26 @@ import argparse
 import pandas as pd
 
 
+def main():
+    parser = argparse.ArgumentParser(description="You can point output files names")
+    parser.add_argument("-hse", type=str, default="hse.json", dest="hse_path",
+                        help="path to msu teacher's list")
+    parser.add_argument("-msu", type=str, default="msu.json", dest="msu_path",
+                        help="path to msu teacher's list")
+    args = parser.parse_args()
+
+    hse_teachers = get_hse_teachers()
+    msu_teachers = get_msu_teachers()
+
+    with codecs.open(args.hse_path, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(hse_teachers, ensure_ascii=False))
+
+    with codecs.open(args.msu_path, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(msu_teachers, ensure_ascii=False))
+
+    get_intersection(hse_teachers, msu_teachers)
+
+
 def levenshtein_distance(a, b):
     "Calculates the Levenshtein distance between a and b. from wikipedia"
     n, m = len(a), len(b)
@@ -29,131 +49,142 @@ def levenshtein_distance(a, b):
 
 
 def get_hse_teachers():
-	# сделаем GET-запрос к страничке преподавателей Вышки с фильтром ФКН
-	url = 'https://www.hse.ru/org/persons/?ltr=%D0%92%D1%81%D0%B5;udept=120026365'
-	r = requests.get(url)
+    # сделаем GET-запрос к страничке преподавателей Вышки с фильтром ФКН
+    url = 'https://www.hse.ru/org/persons/?ltr=%D0%92%D1%81%D0%B5;udept=120026365'
+    r = requests.get(url)
 
-	# обработаем полученный текст, выделяя основные компоненты
-	main_components = '<div class="posts persons">' + r.text.split('<div class="posts persons">', 1)[1]
-	main_components = main_components.split('</div></div></div></div><div class="footer">', 1)[0]
+    # обрабатываем полученный текст, выделяя основные компоненты
+    main_components = '<div class="posts persons">' + r.text.split('<div class="posts persons">', 1)[1]
+    main_components = main_components.split('</div></div></div></div><div class="footer">', 1)[0]
 
-	all_items = []
-	for idx, s in enumerate(main_components.split('</div>\n\n<div class="post person">')):
-	    splitted = s.split('<div class="l-extra small">')
-	    if len(splitted) > 1:
-	        first_part, second_part = splitted[1].split('</div>', 1)
-	    else:
-	        first_part = splitted[0].split('</div>', 1)
-	        second_part = first_part
-	    d = {}
-	    # номера телефонов
-	    d["phones"] = list(map(lambda s: s[6:][:-7], re.findall(r'<span>.*</span>', first_part)))
-	    # адреса электронной почты
-	    emails = list(map(lambda s: s[25:][:-6], re.findall(r'<a class="link" data-at=\'.*\'></a>', first_part)))
-	    d["emails"] = list(map(lambda r: ''.join(list(map(lambda s: s.replace("-at-", "@"), eval(r)))), emails))
-	    # ссылка на страницу преподавателя
-	    d["page_id"] = re.search(r'(/org/persons/\d*|/staff/\w*)', second_part).group(1)
-	    # его имя
-	    d["name"] = re.search(r'title="([\s\w.,\-\(\)]*)"', second_part).group(1)
-	    # должность
-	    d["post"] = list(map(lambda x: x.rstrip(":"), re.findall(r'<span>\s*([^<\n\t]*)\s*<', second_part)))
-	    # подразделение
-	    d["depart"] = ' / '.join(re.findall(r'<a[^>]*>([^<]*)</a>|</span>', 
-						second_part.split('class="tag"', 1)[0])
-				    ).replace('/  /', ';').rstrip("/ ")
-	    # интересы
-	    try:
-	        d["interests"] = ', '.join(re.findall(r'<a[^>]*>([^<]*)</a>', second_part.split('class="tag"', 1)[1]))
-	    except:
-	        d["interests"] = ''
-	    # обработка какого-то странного бага, из-за которого запись разрывается на две
-	    if len(all_items) > 0 and all_items[-1]["name"] == d["name"] and all_items[-1]["page_id"] == d["page_id"] and \
-	            all_items[-1]["post"] == d["post"] and all_items[-1]["depart"] == d["depart"]:
-	        if d["emails"] and d["phones"] and (not all_items[-1]["phones"] and not all_items[-1]["phones"]):
-	            all_items[-1]["emails"] = d["emails"]
-	            all_items[-1]["phones"] = d["phones"]
-	        elif all_items[-1]["emails"] and all_items[-1]["phones"] and (not d["phones"] and not d["phones"]):
-	            continue
-	    else:
-	        all_items.append(d)
-	    # print(idx, d["name"])
-	return all_items
+    all_items = []
+    for s in main_components.split('</div>\n\n<div class="post person">'):
+        splitted = s.split('<div class="l-extra small">')
+        if len(splitted) > 1:
+            first_part, second_part = splitted[1].split('</div>', 1)
+        else:
+            first_part = splitted[0].split('</div>', 1)[0]
+            second_part = first_part
+
+        cur_item = get_hse_item(first_part, second_part)
+
+        # обработка какого-то странного бага, из-за которого запись разрывается на две
+        last_item = all_items[-1] if len(all_items) > 0 else {}
+        if (len(all_items) > 0 and last_item["name"] == cur_item["name"] and
+                last_item["page_id"] == cur_item["page_id"] and 
+                last_item["post"] == cur_item["post"] and 
+                last_item["depart"] == cur_item["depart"]):
+            if (cur_item["emails"] and cur_item["phones"] and 
+                    (not last_item["phones"] and not last_item["phones"])):
+                last_item["emails"] = cur_item["emails"]
+                last_item["phones"] = cur_item["phones"]
+            elif (last_item["emails"] and last_item["phones"] and 
+                    (not cur_item["phones"] and not cur_item["phones"])):
+                continue
+        else:
+            all_items.append(cur_item)
+    return all_items
+
+
+def get_hse_item(first_part, second_part):
+    item = {}
+    # номера телефонов
+    item["phones"] = list(map(lambda s: s[6:][:-7], 
+        re.findall(r'<span>.*</span>', first_part)))
+    # адреса электронной почты
+    emails = list(map(lambda s: s[25:][:-6], 
+        re.findall(r'<a class="link" data-at=\'.*\'></a>', first_part)))
+    item["emails"] = list(map(lambda r: 
+        ''.join(list(map(lambda s: s.replace("-at-", "@"), eval(r)))), emails))
+    # ссылка на страницу преподавателя
+    item["page_id"] = re.search(r'(/org/persons/\d*|/staff/\w*)', second_part).group(1)
+    # его имя
+    item["name"] = re.search(r'title="([\s\w.,\-\(\)]*)"', second_part).group(1)
+    # должность
+    item["post"] = list(map(lambda x: x.rstrip(":"), 
+        re.findall(r'<span>\s*([^<\n\t]*)\s*<', second_part)))
+    # подразделение
+    item["depart"] = ' / '.join(
+        re.findall(r'<a[^>]*>([^<]*)</a>|</span>', 
+        second_part.split('class="tag"', 1)[0])
+        ).replace('/  /', ';').rstrip("/ ")
+    # интересы
+    item["interests"] = ''
+    try:
+        item["interests"] = ', '.join(re.findall(r'<a[^>]*>([^<]*)</a>', 
+            second_part.split('class="tag"', 1)[1]))
+    except IndexError:
+        pass
+
+    return item
+
 
 def get_msu_teachers():
-	all_items = []
-	for i in range(ord('а'), ord('а') + 32):
-	    # на сайте ВМК страницы с разбиением по буквам
-	    url = f'https://cs.msu.ru/persons/all/{chr(i)}'
-	    r = requests.get(url)
-	    if not r.ok:
-	        continue
-	    table = r.text[r.text.find('table') : r.text.rfind('table')]
-	    table_lines = table.split('</tr>')[:-1]
-	    for line in table_lines:
-	        d = {}
-	        ret = re.search(r'<a href="([\d\w/-]*)">([^<]*)</a>', line)
-	        # ссылка на страницу сотрудника
-	        d['page_id'] = ret.group(1)
-	        # его имя
-	        d['name'] = ret.group(2)
-	        # и должность
-	        try:
-	            ret1 = re.search(r'<p>(.*)</p>', line)
-	            d['post'] = ''.join([s for idx, s in enumerate(re.split(r'<|>', ret1.group(1))) if idx % 2 == 0])
-	        except:
-	            try:
-	                ret1 = re.search(r'<div>(.*)</div>', line)
-	                d['post'] = ''.join([s for idx, s in enumerate(re.split(r'<|>', ret1.group(1))) if idx % 2 == 0])
-	            except:
-	                d['post'] = ''
-	        all_items.append(d)
-	return all_items
+    all_items = []
+    for i in range(ord('а'), ord('а')+32):
+        # на сайте ВМК страницы с разбиением по буквам
+        url = 'https://cs.msu.ru/persons/all/'+chr(i) 
+        r = requests.get(url)
+        if not r.ok:
+            continue
+        table = r.text[r.text.find('table') : r.text.rfind('table')]
+        table_lines = table.split('</tr>')[:-1]
+        for line in table_lines:
+            cur_item = {}
+            ret = re.search(r'<a href="([\d\w/-]*)">([^<]*)</a>', line)
+            # ссылка на страницу сотрудника
+            cur_item['page_id'] = ret.group(1)
+            # его имя
+            cur_item['name'] = ret.group(2)
+            # и должность
+            try:
+                ret1 = re.search(r'<p>(.*)</p>', line)
+                cur_item['post'] = ''.join(
+                    [s for idx, s in enumerate(re.split(r'<|>', ret1.group(1))) 
+                    if idx % 2 == 0]
+                )
+            except:
+                try:
+                    ret1 = re.search(r'<div>(.*)</div>', line)
+                    cur_item['post'] = ''.join(
+                        [s for idx, s in enumerate(re.split(r'<|>', ret1.group(1))) 
+                        if idx % 2 == 0]
+                    )
+                except (IndexError, AttributeError):
+                    cur_item['post'] = ''
+            all_items.append(cur_item)
+    return all_items
+
+
+def get_intersection(hse_teachers, msu_teachers):
+    # так сколько же пересечений?
+    df1, df2 = pd.DataFrame(hse_teachers), pd.DataFrame(msu_teachers)
+    # df2['name'] = df2['name'].apply(lambda x: x.strip())
+    joined = df1.join(df2.set_index('name'), on='name', how='inner', lsuffix='_hse', rsuffix='_msu')
+    print("Общих преподавателей с ФКН и ВМК", len(joined))
+    print('\n'.join(list(joined['name'])))
+    # На самом деле на ФКН два Черновых Александра Владимировича и один из них - не наш:(
+
+    # А что, если была где-то небольшая опечатка?
+    leven3 = []
+    for name in hse_teachers:
+        for possib in msu_teachers:
+            if levenshtein_distance(name["name"], possib['name']) < 3:
+                leven3.append('\t\t'.join((name["name"], possib['name'])))
+    
+    print("\nС расстоянием Левенштейна < 3", len(leven3))
+    print('\n'.join(leven3))
+
+    print("\nПомимо Грушо-Глушко добавились три новых абсолютно одинаковых на первый взгляд человека")
+
+    # Их стало больше!!!
+    # А дело все в том, что кто-то криво извлекал имена и они могли заканчиваться на пробелы, как в данном случае
+    # строка 162 спасет горе-программистов
+
+    # Как правильно объединить по ключам, которые немного отличаются?
+    # ведь мы заранее не знаем, какие еще могут быть ошибки в данных (или в программисте, извлекающем их)
+    # узнаем из второго задания...
 
 
 if __name__ == "__main__":
-
-	parser = argparse.ArgumentParser(description="You can point output files names")
-	parser.add_argument("-hse", type=str, default="hse.json", dest="hse_path",
-	                    help="path to msu teacher's list")
-	parser.add_argument("-msu", type=str, default="msu.json", dest="msu_path",
-	                    help="path to msu teacher's list")
-	
-	args = parser.parse_args()
-
-	hse_teachers = get_hse_teachers()
-	msu_teachers = get_msu_teachers()
-
-	with codecs.open(args.hse_path, 'w', encoding='utf-8') as f:
-	    f.write(json.dumps(hse_teachers, ensure_ascii=False))
-
-	with codecs.open(args.msu_path, 'w', encoding='utf-8') as f:
-	    f.write(json.dumps(msu_teachers, ensure_ascii=False))
-
-	# так сколько же пересечений?
-	df1, df2 = pd.DataFrame(hse_teachers), pd.DataFrame(msu_teachers)
-	# df2['name'] = df2['name'].apply(lambda x: x.strip())
-	joined = df1.join(df2.set_index('name'), on='name', how='inner', lsuffix='_hse', rsuffix='_msu')
-	print("Общих преподавателей с ФКН и ВМК", len(joined))
-	print("\n".join(list(joined['name'])))
-	# На самом деле на ФКН два Черновых Александра Владимировича и один из них - не наш:(
-
-	# А что, если была где-то небольшая опечатка?
-	leven3 = []
-	for name in hse_teachers:
-	    for possib in msu_teachers:
-	        if levenshtein_distance(name["name"], possib['name']) < 3:
-	            leven3.append('\t\t'.join((name["name"], possib['name'])))
-	
-	print("\nС расстоянием Левенштейна < 3", len(leven3))
-	print("\n".join(leven3))
-
-	print("\nПомимо Грушо-Глушко добавились три новых абсолютно одинаковых на первый взгляд человека")
-
-	# Их стало больше!!! 
-	# А дело все в том, что кто-то криво извлекал имена и они могли заканчиваться на пробелы, как в данном случае
-	# строка 134 спасет горе-программистов
-
-	# Как правильно объединить по ключам, которые немного отличаются?
-	# ведь мы заранее не знаем, какие еще могут быть ошибки в данных (или в программисте, извлекающем их)
-	# узнаем из второго задания...
-
+    main()
--- integration.py
+++ integration.py
@@ -7,113 +7,120 @@ import random
 import argparse
 import codecs
 
-if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description="You can point output files names")
-	parser.add_argument("-hse", type=str, default="hse.json", dest='hse_path',
-	                    help="path to msu teacher's list")
-	parser.add_argument("-msu", type=str, default="msu.json", dest='msu_path',
-	                    help="path to msu teacher's list")
-	parser.add_argument("-answer", type=str, default="answer.json", dest='answer_path',
-	                    help="path to msu teacher's list")
-	
-	args = parser.parse_args()
-	# загружаем данные
-	hse = json.loads(open(args.hse_path, encoding='utf-8').read())
-	msu = json.loads(open(args.msu_path, encoding='utf-8').read())
+def main():
+    parser = argparse.ArgumentParser(description="You can point output files names")
+    parser.add_argument("-hse", type=str, default="hse.json", dest='hse_path',
+                        help="path to hse teacher's list")
+    parser.add_argument("-msu", type=str, default="msu.json", dest='msu_path',
+                        help="path to msu teacher's list")
+    parser.add_argument("-answer", type=str, default="answer.json", dest='answer_path',
+                        help="path to answer file")
+    
+    args = parser.parse_args()
+    # загружаем данные
+    hse = json.loads(open(args.hse_path, encoding='utf-8').read())
+    msu = json.loads(open(args.msu_path, encoding='utf-8').read())
 
-	for i in range(len(hse)):
-	    hse[i]['hse_page_id'] = hse[i]['page_id']
-	    del hse[i]['page_id']
-	    hse[i]['hse_post'] = hse[i]['post']
-	    del hse[i]['post']
-	for i in range(len(msu)):
-	    msu[i]['msu_page_id'] = msu[i]['page_id']
-	    del msu[i]['page_id']
-	    msu[i]['msu_post'] = msu[i]['post']
-	    del msu[i]['post']
-	# все поля
-	all_keys = set(msu[0].keys()) | set(hse[0].keys())
-	# создаем алфавит, в который входят все символы из встречающихся в именах
-	alphabet = list(set(list(''.join(list(map(lambda x: x['name'], hse))+list(map(lambda x: x['name'], msu))))))
-	alphabet_to = {symbol: i for i, symbol in enumerate(alphabet)}
-	# плохие дяди с вероятностью p могут портить имена: заменять некоторый символ на произвольный из алфавита 
-	p = 0.1
-	for i in range(len(hse)):
-	    if random.random() < p:
-	        pos = random.randint(0, len(hse[i]['name']) - 2)
-	        hse[i]['name'] = hse[i]['name'][:pos] + random.choice(alphabet) + hse[i]['name'][pos+1:]
-	for i in range(len(msu)):
-	    if random.random() < p:
-	        pos = random.randint(0, len(hse[i]['name']) - 2)
-	        msu[i]['name'] = msu[i]['name'][:pos] + random.choice(alphabet) + msu[i]['name'][pos+1:]
-	# имена сотрудников
-	hse_names = list(map(lambda x: x['name'], hse))
-	msu_names = list(map(lambda x: x['name'], msu))
-	# отображение имя -> соответствующие данные о персоне
-	hse_dict = defaultdict(list)
-	for hse_line in hse:
-	    without_name = deepcopy(hse_line)
-	    del without_name['name']
-	    hse_dict[hse_line['name']].append(without_name)
-	    
-	msu_dict = defaultdict(list)
-	for msu_line in msu:
-	    without_name = deepcopy(msu_line)
-	    del without_name['name']
-	    msu_dict[msu_line['name']].append(without_name)
+    rename_columns(hse, 'hse')
+    rename_columns(msu, 'msu')
 
-	# функция, получающая из строки ее представление в виде вектора частот встречания букв
-	def to_num(name):
-	    s = [0 for _ in alphabet]
-	    for x in name:
-	        s[alphabet_to[x]] += 1
-	    return s
+    # все поля
+    all_keys = set(msu[0].keys()) | set(hse[0].keys())
+    # создаем алфавит, в который входят все символы из встречающихся в именах
+    alphabet = list(set(list(''.join(list(map(lambda x: x['name'], hse)) + 
+        list(map(lambda x: x['name'], msu))))))
+    
+    # плохие дяди с вероятностью p могут портить имена: заменять некоторый символ на произвольный из алфавита 
+    corrupt_data(hse, alphabet)
+    corrupt_data(msu, alphabet)
+    
+    # имена сотрудников
+    hse_names = list(map(lambda x: x['name'], hse))
+    msu_names = list(map(lambda x: x['name'], msu))
 
-	# получаем векторизованное представление имён, а также отображение векторов в соответствующие слова
-	num_to_symbol_hse = {}
-	hse_vectors = []
-	for name in hse_names:
-	    name_num = to_num(name)
-	    hse_vectors.append(name_num)
-	    num_to_symbol_hse[' '.join(map(str, name_num))] = name
+    hse_dict = get_map(hse)
+    msu_dict = get_map(msu)
 
-	num_to_symbol_msu = {}
-	msu_vectors = []
-	for name in msu_names:
-	    name_num = to_num(name)
-	    msu_vectors.append(name_num)
-	    num_to_symbol_msu[' '.join(map(str, name_num))] = name
-	    
-	vectors = hse_vectors + msu_vectors
+    num_to_symbol_hse, hse_vectors = get_vectors(hse_names, alphabet)
+    num_to_symbol_msu, msu_vectors = get_vectors(msu_names, alphabet)
+    vectors = hse_vectors + msu_vectors
 
-	# создаём модель кластеризации и кластеризуем данные из двух векторов
-	clustering = MeanShift(bandwidth=1.5).fit(vectors)
+    # создаём модель кластеризации и кластеризуем данные из двух векторов
+    clustering = MeanShift(bandwidth=1.5).fit(vectors)
 
-	# каждый кластер будет записью в объединенной таблице
-	full_table = [{key: None for key in all_keys} for _ in clustering.cluster_centers_]
-	for vector, label in zip(vectors, clustering.labels_):
-	    vector_str = ' '.join(map(str, vector))
-	    # делаем if не с else, а последовательно, т.к. объединяем поля для Complement Union
-	    if vector_str in num_to_symbol_hse:
-	        full_table[label]['name'] = num_to_symbol_hse[vector_str]
-	        cur_dict = hse_dict[num_to_symbol_hse[vector_str]][0]
-	        for key in cur_dict:
-	            full_table[label][key] = cur_dict[key]
-	    # если ключи отличаются самую малость (например, ошибка обработки, 
-	    # доставшаяся из 1 части задания, или вследствие порчи данных плохими дядями),
-	    # то будем указывать оба, ибо понять какой правивльный нетривиально
-	    if vector_str in num_to_symbol_msu:
-	        if full_table[label].get('name'):
-	            if full_table[label]['name'] != num_to_symbol_msu[vector_str]:
-	                full_table[label]['name'] += ' (' + num_to_symbol_msu[vector_str] +  ')'
-	        else:
-	            full_table[label]['name'] = num_to_symbol_msu[vector_str]
-	        cur_dict = msu_dict[num_to_symbol_msu[vector_str]][0]
-	        for key in cur_dict:
-	            full_table[label][key] = cur_dict[key]
+    # каждый кластер будет записью в объединенной таблице
+    full_table = [{key: None for key in all_keys} for _ in clustering.cluster_centers_]
+    for vector, label in zip(vectors, clustering.labels_):
+        vector_str = ' '.join(map(str, vector))
+        # делаем if не с else, а последовательно, т.к. объединяем поля для Complement Union
+        if vector_str in num_to_symbol_hse:
+            full_table[label]['name'] = num_to_symbol_hse[vector_str]
+            for key, value in hse_dict[num_to_symbol_hse[vector_str]][0].items():
+                full_table[label][key] = value
 
-	codecs.open(args.answer_path, 'w', encoding='utf-8').write(json.dumps(full_table, ensure_ascii=False))
+        # если ключи отличаются самую малость (например, ошибка обработки, 
+        # доставшаяся из 1 части задания, или вследствие порчи данных плохими дядями),
+        # то будем указывать оба, ибо понять какой правильный нетривиально
+        if vector_str in num_to_symbol_msu:
+            if full_table[label].get('name'):
+                if full_table[label]['name'] != num_to_symbol_msu[vector_str]:
+                    full_table[label]['name'] += f" ({num_to_symbol_msu[vector_str]})"
+            else:
+                full_table[label]['name'] = num_to_symbol_msu[vector_str]
+            for key, value in msu_dict[num_to_symbol_msu[vector_str]][0].items():
+                full_table[label][key] = value
+
+    codecs.open(args.answer_path, 'w', encoding='utf-8').write(json.dumps(full_table, ensure_ascii=False))
+
+
+def rename_columns(data, university):
+    for i in range(len(data)):
+        data[i][f'{university}_page_id'] = data[i]['page_id']
+        del data[i]['page_id']
+        data[i][f'{university}_post'] = data[i]['post']
+        del data[i]['post']
+
+
+def corrupt_data(data, alphabet, p=0.1):
+    for i in range(len(data)):
+        if random.random() < p:
+            pos = random.randint(0, len(data[i]['name']) - 2)
+            data[i]['name'] = ''.join([
+                data[i]['name'][:pos],
+                random.choice(alphabet),
+                data[i]['name'][pos + 1:]]
+            )
+
+
+def get_map(data):
+    '''возвращает отображение имя -> соответствующие данные о персоне'''
+    data_dict = defaultdict(list)
+    for hse_line in data:
+        without_name = deepcopy(hse_line)
+        del without_name['name']
+        data_dict[hse_line['name']].append(without_name)
+    return data_dict
+
+
+def to_num(name, alphabet):
+    '''получает из строки ее представление в виде вектора частот встречания букв'''
+    alphabet_to = {symbol: i for i, symbol in enumerate(alphabet)}
+    s = [0 for _ in alphabet]
+    for letter in name:
+        s[alphabet_to[letter]] += 1
+    return s
+
+
+def get_vectors(names, alphabet):
+    '''получает векторизованное представление имён и отображение векторов в слова'''
+    num_to_symbol, vectors = {}, []
+    for name in names:
+        name_num = to_num(name, alphabet)
+        vectors.append(name_num)
+        num_to_symbol[' '.join(map(str, name_num))] = name
+    return num_to_symbol, vectors
+
 
 '''
 Пример выходных данных:
@@ -131,3 +138,6 @@ if __name__ == "__main__":
 При этом производится "Outer union" и Complementation
 '''
 
+
+if __name__ == "__main__":
+    main()
